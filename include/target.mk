365f4a54cd0595ac0f6b207faae35f48e35d2183
aa4851698 <sgwapk@163.com>
2021-02-24 17:32:18 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -15,7 +15,10 @@ DEVICE_TYPE?=router
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd \
 block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget libustream-openssl ca-certificates \
-default-settings luci-app-filetransfer luci-app-upnp luci-app-autoreboot luci-app-webadmin \
+default-settings luci luci-app-ddns luci-app-upnp luci-app-autoreboot luci-app-webadmin \
+luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic \
+luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree \
+luci-app-sfe luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
 ddns-scripts_aliyun ddns-scripts_dnspod
 # For the basic set
 DEFAULT_PACKAGES.basic:=
@@ -23,6 +26,7 @@ DEFAULT_PACKAGES.basic:=
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
 DEFAULT_PACKAGES.router:=dnsmasq-full iptables ppp ppp-mod-pppoe firewall
+
 
 ifneq ($(DUMP),)
   all: dumpinfo
